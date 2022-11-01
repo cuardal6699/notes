df658e07b5fd6ca3ddea820622f82b32a43c78b9
cuardal6699 <ixes6699@gmail.com>
2022-11-01 13:17:49 +0500

    Notes delete add update working

---
+++ pages/api/note/[id].ts
@@ -0,0 +1,17 @@
+import { prisma } from "../../../lib/prisma";
+import { NextApiRequest } from "next";
+import { NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    const noteId = req.query.id
+
+    if(req.method === 'DELETE') {
+        const note = await prisma.note.delete({
+            where: {id: String(noteId)}
+        })
+        res.json(note)
+    }else {
+        console.log("Note could not be created");
+    }
+    
+}
